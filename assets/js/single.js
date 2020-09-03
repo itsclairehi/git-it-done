e76d3941fdb44d9a2fa017c2a2b98eec16cab61c
claire p <cpuckett9@gmail.com>
2020-09-03 17:38:41 -0500

    error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    //grab repo name from url query string
+    var queryString = document.location.search
+    var repoName = queryString.split("=")[1]
+
+    if (repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName
+
+        getRepoIssues(repoName)
+    } else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -14,12 +31,12 @@ var getRepoIssues = function (repo) {
                     displayWarning(repo)
                 }
             });
+        } else {
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html")
         }
     })
 }
-
-
-getRepoIssues("facebook/react");
 
 var displayIssues = function (issues) {
     if (issues.length === 0) {
@@ -69,5 +86,7 @@ var displayWarning = function (repo) {
 
     // append to warning container
     limitWarningEl.appendChild(linkEl);
-    
+
 };
+
+getRepoName()
